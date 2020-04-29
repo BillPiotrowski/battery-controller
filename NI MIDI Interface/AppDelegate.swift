7d115345653098cd7fbda0eb142895c8913a8cce
Bill Piotrowski <Bill Piotrowski>
2020-04-29 14:23:24 -0700

    Initial Commit

---
+++ NI MIDI Interface/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  NI MIDI Interface
+//
+//  Created by William Piotrowski on 4/29/20.
+//  Copyright © 2020 William Piotrowski. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ NI MIDI Interface/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  NI MIDI Interface
+//
+//  Created by William Piotrowski on 4/29/20.
+//  Copyright © 2020 William Piotrowski. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
