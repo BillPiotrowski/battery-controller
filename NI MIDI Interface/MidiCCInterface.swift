5faa0cb2a32412de49a1766e70570fb3c85a5ec0
Bill Piotrowski <billpiotrowski@me.com>
2026-07-19 20:08:15 -0700

    Add the instrument and controller config files add readme and update cluade.md Make notes on input CC functionality.

--- NI MIDI Interface/MidiCCInterface.swift
+++ NI MIDI Interface/MidiCCInterface.swift
@@ -23,11 +23,17 @@ enum MidiInputMapping: Int {
     case decay = 22
     case sustain = 23
     case release = 24
+
+    // not working??
     case enableAttackEnvelope = 52
+
     case transientAttack = 27
     case transientSustain = 28
     case enableTransientMaster = 53
+
+    // Not working??
     case tune = 26
+
     case fineTune = 46
     case lofiBits = 31
     case lofiHertz = 32
@@ -38,22 +44,29 @@ enum MidiInputMapping: Int {
     case reverbSend = 36
     case delaySend = 37
     case velocity = 38
+
+    // Not sure this is connected or what it is?
     case envOrder = 41
+
     case formant = 42
     case mute = 62
     case solo = 63
     case unsoloAll = 67
+
+    // Not confident locks are working as intended.
     case lock = 64
     case lockAll = 65
     case unlockAll = 66
+
     case select = 68
     case copy = 43
     case paste = 44
     case reset = 61
     case resetAll = 69
     case undo = 73
-    case redo = 74
+    case redo = 74  
     
+    // Not confident this is working.
     case loopStart = 81
     case loopStartFine = 82
     case loopLength = 83
