de8abbf7458f0671164593dc6c0cf152dd4bb332
Bill Piotrowski <billpiotrowski@me.com>
2026-07-22 11:27:10 -0700

    Remove reflection for cell.parameter in undo group name.

--- NI MIDI Interface/Engine/UndoCoordinator.swift
+++ NI MIDI Interface/Engine/UndoCoordinator.swift
@@ -22,7 +22,7 @@ final class UndoCoordinator {
 private extension UndoCoordinator {
     struct UndoGroup: Equatable {
         enum Task: Equatable {
-            case parameter(String)
+            case parameter(ParameterKey)
             case reset
             case paste
             case resetAll
@@ -32,10 +32,29 @@ private extension UndoCoordinator {
         // TODO: add a timer or something to expire this after a few moments.
     }
 
+    // Unique Id to be applied to Cell.Parameter so it can generate unique undo group names.
+    enum ParameterKey: Equatable {
+        // Property
+        case start1, start2, volume, pan, speedCoarse, speedFine,
+            filterLow, filterHigh, transientAttack, transientSustain,
+            enableTransientMaster, fineTune, reverbSend, delaySend,
+            velocity, envOrder, formant, loopStart, loopStartFine,
+            loopLength, loopLengthFine
+
+        // Amp Envelope
+        case attack, hold, decay, sustain, release, enableAmpEnvelope
+
+        // Lo-Fi
+        case lofiBits, lofiHertz, lofiNoise, lofiColor, lofiOut, enableLofi
+
+        // Sample
+        case pitch
+    }
+
     static func group(for intent: Intent) -> UndoGroup? {
         switch intent {
         case .updateCellParameter(let cellIndex, let parameter):
-            return UndoGroup(task: .parameter(undoName(parameter)), cellIndex: cellIndex)
+            return UndoGroup(task: .parameter(key(for: parameter)), cellIndex: cellIndex)
         case .reset(let cellIndex):
             return UndoGroup(task: .reset, cellIndex: cellIndex)
         case .paste:
@@ -47,9 +66,50 @@ private extension UndoCoordinator {
         }
     }
 
-    // TODO - eventually want this to be an internal enum like Task. Reflection should not be used here.
-    static func undoName(_ parameter: Cell.Parameter) -> String {
-        return Mirror(reflecting: parameter).children.first?.label ?? "\(parameter)"
+    static func key(for parameter: Cell.Parameter) -> ParameterKey {
+        switch parameter {
+        // Property
+        case .start1: return .start1
+        case .start2: return .start2
+        case .volume: return .volume
+        case .pan: return .pan
+        case .speedCoarse: return .speedCoarse
+        case .speedFine: return .speedFine
+        case .filterLow: return .filterLow
+        case .filterHigh: return .filterHigh
+        case .transientAttack: return .transientAttack
+        case .transientSustain: return .transientSustain
+        case .enableTransientMaster: return .enableTransientMaster
+        case .fineTune: return .fineTune
+        case .reverbSend: return .reverbSend
+        case .delaySend: return .delaySend
+        case .velocity: return .velocity
+        case .envOrder: return .envOrder
+        case .formant: return .formant
+        case .loopStart: return .loopStart
+        case .loopStartFine: return .loopStartFine
+        case .loopLength: return .loopLength
+        case .loopLengthFine: return .loopLengthFine
+
+        // Amp Envelope
+        case .attack: return .attack
+        case .hold: return .hold
+        case .decay: return .decay
+        case .sustain: return .sustain
+        case .release: return .release
+        case .enableAmpEnvelope: return .enableAmpEnvelope
+
+        // Lo-Fi
+        case .lofiBits: return .lofiBits
+        case .lofiHertz: return .lofiHertz
+        case .lofiNoise: return .lofiNoise
+        case .lofiColor: return .lofiColor
+        case .lofiOut: return .lofiOut
+        case .enableLofi: return .enableLofi
+
+        // Sample
+        case .pitch: return .pitch
+        }
     }
 }
 
